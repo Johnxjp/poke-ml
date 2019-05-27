725238732de9d810cb0fb867ffb8eebcf2cd63da
John Lingi <lingijohn23@gmail.com>
2019-05-27 21:30:14 +0100

    Added greyscale option to image loader

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,21 @@ import numpy as np
 from PIL import Image
 
 
-def load_images(root_dir):
-    """Loads all images in a numpy array. All images are given as 40x40"""
+def load_images(root_dir, color=False):
+    """
+    Loads all images in a numpy array. All images are given as 40x40 and values
+    between 0-1.
+    """
     files = os.listdir(root_dir)
     print(f"Total number of files: {len(files)}")
 
     max_size = 40
-    images = np.zeros((len(files), max_size, max_size, 3), dtype=np.float32)
+
+    if color:
+        images = np.zeros((len(files), max_size, max_size, 3), dtype=np.float32)
+    else:
+        images = np.zeros((len(files), max_size, max_size), dtype=np.float32)
+
     filenames = []
     for ind, file in enumerate(files):
         original_image = Image.open(f"{root_dir}/{file}").convert('RGBA')
@@ -18,7 +26,7 @@ def load_images(root_dir):
         # Convert to white background
         new_image = Image.new('RGBA', original_image.size, "white")
         new_image.paste(original_image, mask=original_image)
-        im = new_image.convert('RGB')
+        im = new_image.convert('RGB' if color else 'L')
         # Crop image if to large. All images will be 40x40
         if im.size[0] > max_size:
             top_left = (max_size - im.size[0]) // 2
